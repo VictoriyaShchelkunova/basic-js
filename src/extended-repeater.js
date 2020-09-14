dda3b0400deaa3f7613d0301ccf50bcf4cc787e4
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2020-09-14 12:40:28 +0300

    feat: add a solution of the extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.separator === undefined) {
+    options.separator = '+';
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  }
+  if (options.addition === undefined) {
+    options.addition = '';
+  }
+  let resultString = '';
+  let repeatedString = '';
+  str = String(str);
+  options.addition = String(options.addition);
+  if (options.repeatTimes === undefined && options.additionRepeatTimes) {
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      repeatedString += options.addition + options.additionSeparator;
+    }
+    return str + repeatedString.slice(0, -2);
+  };
+
+  if (options.additionRepeatTimes === undefined && options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      if (options.addition) {
+        resultString += str + options.addition + options.additionSeparator + options.separator;
+      } else {
+        resultString += str + options.separator;
+      }
+    }
+    return resultString.slice(0, -options.separator.length);
+  }
+
+  if (options.repeatTimes === undefined && options.additionRepeatTimes === undefined) {
+    return str + options.addition;
+  } else {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      for (let j = 0; j < options.additionRepeatTimes; j++) {
+        repeatedString += options.addition + options.additionSeparator;
+      }
+      resultString += str + repeatedString.slice(0, -options.additionSeparator.length) + options.separator;
+      repeatedString = '';
+    }
+  }
+  return resultString.slice(0, -options.separator.length);
 };
-  
