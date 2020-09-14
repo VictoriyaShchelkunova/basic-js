ebb8c5fc47a6a26c117e9c1ba443f03b2eac95f4
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2020-09-14 21:31:57 +0300

    feat: add a solution of the vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,110 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    this.value = value;
+    this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    }
+    key = key.toLowerCase();
+    let stringMessage = message.toLowerCase().split('').filter(item => this.alphabet.indexOf(item) !== -1).join('');
+    let def = stringMessage.length - key.length;
+
+    if (def > 0 && def < key.length) {
+      key = key + key.slice(0, def);
+    } else if (def > key.length) {
+      for (let i = 0; i < def; i++) {
+        key += key[i];
+      }
+    } else if (def < 0) {
+      key = key.slice(0, stringMessage.length);
+    }
+
+    let currentKey = [];
+    for (let i = 0, j = 0; i < message.length, j < key.length; i++, j++) {
+      if (this.alphabet.indexOf(message.toLowerCase()[i]) !== -1) {
+        currentKey[i] = key[j];
+      } else {
+        currentKey[i] = message.toLowerCase()[i];
+        j--;
+      }
+    }
+
+    let strResult = [];
+    for (let i = 0; i < message.length; i++) {
+      let messageCurrent = message.toLowerCase();
+      let indexString = this.alphabet.indexOf(messageCurrent[i]);
+      
+      if (indexString !== -1) {
+        strResult[i] = this.alphabet.find((elem, idx) => {
+          let currentIndex = indexString + this.alphabet.indexOf(currentKey[i]);
+          if (currentIndex >= 26) {
+            currentIndex = currentIndex - 26;
+          };
+          return idx === currentIndex;
+        })
+      } else {
+        strResult[i] = messageCurrent[i];
+      }
+    }
+    if (this.value === false) {
+      return strResult.reverse().join('').toUpperCase();
+
+    }
+    return strResult.join('').toUpperCase();
+    
+  }
+  decrypt(encrypteMessage, key) {
+    if (encrypteMessage === undefined || key === undefined) {
+      throw new Error();
+    }
+    key = key.toLowerCase();
+    let stringEncrypteMessage = encrypteMessage.toLowerCase().split('').filter(item => this.alphabet.indexOf(item) !== -1).join('');
+    let def = stringEncrypteMessage.length - key.length;
+    if (def > 0 && def < key.length) {
+      key = key + key.slice(0, def);
+    } else if (def >= key.length) {
+      for (let i = 0; i < def; i++) {
+        key += key[i];
+      }
+    } else if (def < 0) {
+      key = key.slice(0, stringEncrypteMessage.length);
+    }
+    let currentKey = [];
+    for (let i = 0, j = 0; i < encrypteMessage.length, j < key.length; i++, j++) {
+      if (this.alphabet.indexOf(encrypteMessage.toLowerCase()[i]) !== -1) {
+        currentKey[i] = key[j];
+      } else {
+        currentKey[i] = encrypteMessage.toLowerCase()[i];
+        j--;
+      }
+    }
+    let strResult = [];
+    for (let i = 0; i < encrypteMessage.length; i++) {
+      let messageCurrent = encrypteMessage.toLowerCase();
+      let indexString = this.alphabet.indexOf(messageCurrent[i]);
+      if (indexString !== -1) {
+        strResult[i] = this.alphabet.find((elem, idx) => {
+          let currentIndex = indexString - this.alphabet.indexOf(currentKey[i]);
+          if (currentIndex < 0) {
+            currentIndex = currentIndex + 26;
+          };
+          return idx === currentIndex;
+        })
+      } else {
+        strResult[i] = messageCurrent[i];
+      }
+    }
+    if (this.value === false) {
+      return strResult.reverse().join('').toUpperCase();
+    }
+    
+    return strResult.join('').toUpperCase();
+
   }
 }
 
